3b0e4170d9ed43cdb4a2ef8033ac2612840c2bcb
Filip Troníček <filip.tronicek@seznam.cz>
2020-04-04 19:44:22 +0200

    Add a max-length

--- lib/main.dart
+++ lib/main.dart
@@ -14,38 +14,51 @@ class MyApp extends StatelessWidget {
         primaryColor: darkGrey,
         brightness: Brightness.light,
       ),
-      darkTheme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(primaryColor: darkGrey, ),
       home: Scaffold(
           backgroundColor: orange,
           appBar: AppBar(title: Text('Interclip mobile')),
           body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-           //   Text("First line"),
-           //   Text("second line lmao"),
+              //   Text("First line"),
+              //   Text("second line lmao"),
               TextFormField(
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter username.';
-                  } else if(value.length > 5) {
-                    return 'Code too long';
-                  }
-                  return null;
-                },
-                textAlign: TextAlign.center,
-                textAlignVertical: TextAlignVertical.center,
-                maxLines: 1,
-                decoration: InputDecoration(
-                    border: InputBorder.none, labelText: 'Enter your code'),
-              ),
-              
+                  maxLength: 5,
+                  maxLengthEnforced: true,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter username.';
+                    } else if (value.length > 5) {
+                      return 'Code too long';
+                    } else {
+                       return validateMyInput(value);
+                    }
+                  },
+                  textAlign: TextAlign.center,
+                  textAlignVertical: TextAlignVertical.center,
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                      border: InputBorder.none, labelText: 'Enter your code'),
+                  style: TextStyle(
+                    fontSize: 120,
+                  )),
             ],
           )),
     );
   }
 }
 
+
 Color _colorFromHex(String hexColor) {
   final hexCode = hexColor.replaceAll('#', '');
   return Color(int.parse('FF$hexCode', radix: 16));
 }
+String validateMyInput(String value) {
+    Pattern pattern = r'^\d+(?:\.\d+)?$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value))
+      return 'Enter Valid Number';
+    else
+      return null;
+  }
